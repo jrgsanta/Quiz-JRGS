334a0b440270fa53316329deaad433958fd24b21
jrgsanta <imac@iMac-de-iMac.local>
2015-08-03 00:53:17 +0200

    Corregir error en router.js
    Corregir error en router.js

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-route.get('/quizes',	quizController.index);
-route.get('/quizes/:quizId(\\d+)',	quizController.show);
+router.get('/quizes',	quizController.index);
+router.get('/quizes/:quizId(\\d+)',	quizController.show);
 //router.get('/quizes/question', quizController.question);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', quizController.author);
