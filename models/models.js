491b3596954a160a0a28f8c090f0d1c2d3062f66
jrgsanta <imac@iMac-de-iMac.local>
2015-08-03 01:05:42 +0200

    Modificación prueba models.js

--- models/models.js
+++ models/models.js
@@ -45,12 +45,6 @@ sequelize.sync().then(function(){
 			Quiz.create({ 	pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
 						});
-			Quiz.create({ 	pregunta: 'Capital de Portugal',
-							respuesta: 'Lisboa'
-						});		
-			Quiz.create({ 	pregunta: 'Capital de Francia',
-							respuesta: 'Pariz'
-						});	
 			Quiz.create({ 	pregunta: 'Capital de Alemania',
 							respuesta: 'Berlin'
 						})											
