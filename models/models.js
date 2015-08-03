4a3f9b398ca376affe4989812ce73f7549acd99c
jrgsanta <imac@iMac-de-iMac.local>
2015-08-03 08:14:38 +0200

    Añadir más elementos a la base de datos en la inicialización

--- models/models.js
+++ models/models.js
@@ -45,6 +45,12 @@ sequelize.sync().then(function(){
 			Quiz.create({ 	pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
 						});
+			Quiz.create({ 	pregunta: 'Capital de Portugal',
+							respuesta: 'Lisboa'
+						});
+			Quiz.create({ 	pregunta: 'Capital de Francia',
+							respuesta: 'Paris'
+						});									
 			Quiz.create({ 	pregunta: 'Capital de Alemania',
 							respuesta: 'Berlin'
 						})											
